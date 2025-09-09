26c9bed5941d4c8635ec4727faaef7afd6d3fc33
Akash Guchait <aguchait150@gmail.com>
2025-09-09 13:14:35 +0530

    make changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 
+
+// Time update code
 var timeDisplay = document.getElementById("time2");
 
 function refreshTime() {
@@ -8,423 +10,143 @@ function refreshTime() {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, 
+    hour12: true,
   };
   var timeString = date.toLocaleTimeString("en-US", options);
   timeString = timeString.replace(/ AM| PM/g, "");
   timeDisplay.innerHTML = timeString;
 }
-
 setInterval(refreshTime, 1000);
 
+// GSAP animations with ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
 
-/* gsap for name */
-
-
-
-
+// Name animation
 const tl = gsap.timeline();
-
 tl.from(".portfolio__name h1 span", {
-    y: -150,       
-    opacity: 0,    
-    duration: 1,  
-    stagger: 0.5,  
-    ease: "power4.easeout" 
-});
-
-
-
-
-
-gsap.to(".portfolio__image img", {
+  y: -150,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.5,
+  ease: "power4.out",
   scrollTrigger: {
-    trigger: ".portfolio__image", 
-    scrub: 0.5, 
-  },
-  scale: 1.5,
-  ease: "power4.easeout",
+    trigger: ".portfolio__name",
+    start: "top 80%",
+    toggleActions: "play none none none",
+    invalidateOnRefresh: true,
+  }
 });
 
+// Portfolio image zoom animation
+gsap.to(".portfolio__image img", {
+  scale: 1.5,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: ".portfolio__image",
+    start: "top 80%",
+    scrub: 0.5,
+    invalidateOnRefresh: true,
+  }
+});
 
-
-
-
+// Works heading animation
 gsap.from(".works__heading h1 span", {
   y: 150,
   opacity: 0,
   duration: 1.2,
   stagger: 0.5,
-  ease: "power4.easeIn",
+  ease: "power4.in",
   scrollTrigger: {
-    trigger: ".works__heading h1 span", 
-    start: "top 80%", 
+    trigger: ".works__heading h1 span",
+    start: "top 80%",
     toggleActions: "play none none none",
-  },
-});
-
-
-
-
-  let animation = gsap.from(".work1", {
-    opacity: 0, 
-    x: -1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work1", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
-
-
-  
-
-
-
-
-
-gsap.from(".work2", {
-    opacity: 0, 
-    x: 1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work2", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
-  
-
-
-
-
-
-  gsap.from(".work3", {
-    opacity: 0, 
-    x: -1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work3", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
-
-
-
-
-  gsap.from(".work4", {
-    opacity: 0, 
-    x: 1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work4", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
- 
-
-
-
-
-  gsap.from(".work5", {
-    opacity: 0, 
-    x: -1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work5", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
-
-
-
-
-  gsap.from(".work6", {
-    opacity: 0, 
-    x: 1000,
-    duration: 1.5,
-    ease: "power4.easein",
-    scrollTrigger: {
-      trigger: ".work6", 
-      start: "top 80%", 
-      toggleActions: "play none none none",
-    }
-  });
-
-
-
-
-
-
-const aboutTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".portfolio__about", 
-    start: "top top",             
-    end: "bottom top",           
-    scrub: 5,               
-    pin: true,                                 
+    invalidateOnRefresh: true,
   }
 });
 
+// Work1 animation
+gsap.from(".work1", {
+  opacity: 0,
+  x: -1000,
+  duration: 1.5,
+  ease: "power4.in",
+  scrollTrigger: {
+    trigger: ".work1",
+    start: "top 80%",
+    toggleActions: "play none none none",
+    invalidateOnRefresh: true,
+  }
+});
+
+// Work2 animation
+gsap.from(".work2", {
+  opacity: 0,
+  x: 1000,
+  duration: 1.5,
+  ease: "power4.in",
+  scrollTrigger: {
+    trigger: ".work2",
+    start: "top 80%",
+    toggleActions: "play none none none",
+    invalidateOnRefresh: true,
+  }
+});
+
+// About section timeline animation
+const aboutTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".portfolio__about",
+    start: "top top",
+    end: "bottom top",
+    scrub: 5,
+    pin: true,
+    invalidateOnRefresh: true,
+  }
+});
 
 aboutTimeline.to(".about__heading", {
-  opacity: 0,     
-  scale: 0.5,      
-  duration: 1,     
-  ease: "power4.out",
+  opacity: 0,
+  scale: 0.5,
+  duration: 1,
+  ease: "power4.out"
 }, 0);
 
-
 aboutTimeline.fromTo(".about__image", {
-  opacity: 0,      
-  scale: 2,      
-}, 
-{
-  opacity: 1,      
-  scale: 1,       
-  duration: 1,     
-  ease: "power4.out",
-}, 0.5); 
+  opacity: 0,
+  scale: 2,
+}, {
+  opacity: 1,
+  scale: 1,
+  duration: 1,
+  ease: "power4.out"
+}, 0.5);
 
-
-
-
-
-
-
-
+// CTA section animation
 gsap.from('.cta_content h3', {
   y: 100,
   opacity: 0,
   duration: 1.2,
   stagger: 0.5,
-  ease: "power4.easeIn",
+  ease: "power4.in",
   scrollTrigger: {
-    trigger: '.cta_content h3', 
-    start: "top 80%", 
-    toggleActions: "play none none none" 
-  },
-});
-
-gsap.from('.cta_content p', {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: '.cta_content h3', 
-    start: "top 80%", 
-    toggleActions: "play none none none" 
-  },
-});
-
-gsap.from('.btn2', {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: '.cta_content h3', 
-    start: "top 80%", 
-    toggleActions: "play none none none" 
-  },
-});
-
-
-gsap.from(".expertise__heading h1 span", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".expertise__heading h1 span", 
-    start: "top 80%", 
+    trigger: '.cta_content h3',
+    start: "top 80%",
     toggleActions: "play none none none",
-  },
-});
-
-
-gsap.from(".expertise1", {
-  y: 150,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".expertise1", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-})
-
-gsap.from(".motivation__heading", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".motivation__heading", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-})
-
-gsap.from(".motivation__para p", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".motivation__para p", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-})
-
-gsap.from(".motivation__signature img", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".motivation__signature", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-})
-
-gsap.to(".about2__image img", {
-  scrollTrigger: {
-    trigger: ".about2__image", 
-    scrub: 0.5, 
-  },
-  scale: 1.5,
-  ease: "power4.easeout",
-});
-
-gsap.to(".experience__list", {
-  scrollTrigger: {
-      trigger: ".portfolio__experience",
-      start: "top top", 
-      end: "bottom bottom", 
-      pin: ".motivation__heading", 
-      pinSpacing: true, 
-      scrub: 1,
+    invalidateOnRefresh: true,
   }
 });
 
-ScrollTrigger.create({
-  trigger: ".experience__list",
-  start: "top top", 
-  end: "bottom bottom", 
-  pin: ".motivation__heading", 
-  scrub: 1,
-});
-
-gsap.to(".about3__image img", {
-  scrollTrigger: {
-    trigger: ".about3__image", 
-    scrub: 0.5, 
-  },
-  scale: 1.5,
-  ease: "power4.easeout",
-});
-
-gsap.from(".faq__heading h2", {
-  y: 100,
+// Additional animations can be structured similarly:
+// Example: Footer or other sections
+gsap.from(".footer__links", {
+  y: 50,
   opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
+  duration: 1,
+  ease: "power4.out",
   scrollTrigger: {
-    trigger: ".faq__heading h2", 
-    start: "top 80%", 
+    trigger: ".footer__links",
+    start: "top 90%",
     toggleActions: "play none none none",
-  },
+    invalidateOnRefresh: true,
+  }
 });
-
-
-gsap.from(".accordion-item", {
-  y: 150,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".accordion-item", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-})
-
-gsap.from(".contact__heading h2 span", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".contact__heading h2 span", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-});
-
-gsap.from(".contact__button", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".contact__button", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-});
-
-gsap.from(".contact__image", {
-  y: 100,
-  opacity: 0,
-  duration: 1.2,
-  stagger: 0.5,
-  ease: "power4.easeIn",
-  scrollTrigger: {
-    trigger: ".contact__image", 
-    start: "top 80%", 
-    toggleActions: "play none none none",
-  },
-});
-const lenis = new Lenis({
-  smooth: true,
-  lerp: 0.05,
-  // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  smoothTouch: true,
-});
-
-lenis.on('scroll', (e) => {
-  console.log('Scrolling:', e);
-  
-});
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
